f2ee787e2e28f7cb4be1b8d9d2da43e0599e0d7d
KMSMKarunanayake <surajmayantha150f@gmail.com>
2021-10-05 21:40:33 +0530

    User Registration Initial

---
+++ app/src/main/java/com/example/ecake/RegistrationActivity.java
@@ -0,0 +1,156 @@
+package com.example.ecake.Registration;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.example.ecake.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    private Button signUp;
+    private EditText name,email,phone,password;
+    private TextView signIn;
+    private String userId;
+    private FirebaseAuth auth;
+    private ProgressBar progressBar;
+
+    private FirebaseFirestore fstore;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        auth = FirebaseAuth.getInstance();
+        fstore=FirebaseFirestore.getInstance();
+
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
+        signUp = findViewById(R.id.reg_btn);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email_reg);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password_reg);
+        signIn = findViewById(R.id.sign_in);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+            }
+        });
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                createUser();
+                //validateEmailAddress(email);
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+        });
+    }
+
+    private void createUser() {
+
+        String userName = name.getText().toString();
+        String userEmail = email.getText().toString();
+        String userPhone = phone.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(userName)){
+            Toast.makeText(this, "Please Enter Your Name!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userEmail)){
+            Toast.makeText(this, "Please Enter Your Email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //********Email Validation with  pattern***************
+        if(!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+            Toast.makeText(this, "Please Enter Valid Email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(userPhone)){
+            Toast.makeText(this, "Please Enter Your Phone Number!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //********Email Validation with  pattern***************
+        if(!Patterns.PHONE.matcher(userPhone).matches()){
+            Toast.makeText(this, "Please Enter Correct Phone Number!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+        if (TextUtils.isEmpty(userPassword)){
+            Toast.makeText(this, "Password is Empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userPassword.length() < 6){
+            Toast.makeText(this, "Password Length must be greater then 6 letter", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //Create User
+        auth.createUserWithEmailAndPassword(userEmail,userPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegistrationActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                            userId = auth.getCurrentUser().getUid();
+                            DocumentReference documentReference =fstore.collection("UserProfile").document(userId);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("Name",userName);
+                            user.put("Phone",userPhone);
+                            user.put("Email",userEmail);
+                            //user.put("Password",userPassword);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(RegistrationActivity.this, "Profile Created", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull  Exception e) {
+                                    Toast.makeText(RegistrationActivity.this, "Profile Created", Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                            progressBar.setVisibility(View.GONE);
+
+                        }
+                        else {
+                            progressBar.setVisibility(View.GONE);
+                            Toast.makeText(RegistrationActivity.this, "Error:"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+}
