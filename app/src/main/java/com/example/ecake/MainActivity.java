5206e1a7f3cc2cf618b6d44c4fae37aa0b6f54b0
Sasanka Vikum <58661798+Sasankavikum@users.noreply.github.com>
2021-10-06 20:15:22 +0530

    address inserted

--- app/src/main/java/com/example/ecake/MainActivity.java
+++ app/src/main/java/com/example/ecake/MainActivity.java
@@ -1,109 +1,113 @@
-package com.example.cake;
+package com.example.ecake;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView createNewUserAccount;
-    private TextView loginUser;
-    private EditText loginEmail;
-    private EditText loginPassword;
-    private TextView createAdminAcoount;
-    private Button loginButton;
-    private ProgressDialog dialog;
-
-    private FirebaseAuth auth;
+    //variable
+    private EditText name_input, phone_number_input, address_input, town_input;
+    private Button Button_Save;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dialog = new ProgressDialog(this);
-        auth = FirebaseAuth.getInstance();
 
-//        if (auth.getCurrentUser() != null) {
-//            startActivity(new Intent(getApplicationContext(), AdminHome.class));
-//        }
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Address");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        login();
+        //hooks to all xml elements in address_activity
+        name_input = (EditText) findViewById(R.id.name_input);
+        phone_number_input = (EditText) findViewById(R.id.phone_number_input);
+        address_input = (EditText) findViewById(R.id.address_input);
+        town_input = (EditText) findViewById(R.id.town_input);
+        Button_Save = (Button) findViewById(R.id.save_address_btn);
+
+        db = FirebaseFirestore.getInstance();
+
+        //save data in firebase on button click
+        Button_Save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                InsertAddressData();
+            }
+        });
     }
 
+    private void InsertAddressData(){
+        String name = name_input.getText().toString();
+        String addresss = address_input.getText().toString();
+        String phone = phone_number_input.getText().toString();
+        String town = town_input.getText().toString();
+        String id = UUID.randomUUID().toString();
 
-    private void login() {
-        createNewUserAccount = findViewById(R.id.create_new_account);
-        loginEmail = findViewById(R.id.login_email);
-        loginPassword = findViewById(R.id.login_password);
-        loginButton = findViewById(R.id.login_button);
-        loginUser = findViewById(R.id.login_as_user);
-        createAdminAcoount = findViewById(R.id.create_new_admin_account);
+        if(TextUtils.isEmpty(name)){
+            Toast.makeText(this,"Please write your name..",Toast.LENGTH_SHORT);
+        }
+        else if(TextUtils.isEmpty(phone)){
+            Toast.makeText(this,"Please write your phone number..",Toast.LENGTH_SHORT);
+        }
+        else if(!phone_number_input.getText().toString().matches("[0-9]{10}")) {
+            phone_number_input.setError("Enter valid phone Number");
+        }
+        else if(TextUtils.isEmpty(addresss)){
+            Toast.makeText(this,"Please enter your address..",Toast.LENGTH_SHORT);
+        }
+        else if(TextUtils.isEmpty(town)){
+            Toast.makeText(this,"Please enter your town..",Toast.LENGTH_SHORT);
+        }
+        else {
+            HashMap<String, Object> map =new HashMap<>();
+            map.put("id",id);
+            map.put("address",addresss);
+            map.put("phone",phone);
+            map.put("town",town);
+            map.put("name",name);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = loginEmail.getText().toString().trim();
-                String password = loginPassword.getText().toString().trim();
-
-                if (TextUtils.isEmpty(email)) {
-                    loginEmail.setError("Email is required");
-                    return;
-                }
-                if (TextUtils.isEmpty(password)) {
-                    loginPassword.setError("Password is required");
-                    return;
-                }
-
-                dialog.setMessage("Login in...");
-                dialog.show();
-
-                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            dialog.dismiss();
-                            Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), AdminHome.class));
-                        } else {
-                            dialog.dismiss();
-                            Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+            db.collection("Documents").document(id).set(map)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Data saved", Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
-            }
-        });
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        loginUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), UserLogin.class));
-            }
-        });
-        createNewUserAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), UserRegistration.class));
-            }
-        });
-        createAdminAcoount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),AdminRegister.class));
-            }
-        });
+
+            Intent intent = new Intent(MainActivity.this, AddressConfirmActivity.class);
+            intent.putExtra("keyname",name);
+            intent.putExtra("keyphone",phone);
+            intent.putExtra("keyaddress",addresss);
+            intent.putExtra("keytown",town);
+            intent.putExtra("keyid",id);
+            intent.putExtra("keyTotal",1500);
+            startActivity(intent);
+        }
     }
 }
